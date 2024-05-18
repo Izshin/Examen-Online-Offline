9bd69906499b942db6f97fbecca40b1fa565014d
Izshin <ivanfernandezlimarquez757@gmail.com>
2024-05-18 18:05:25 +0200

    first commit

---
+++ DeliverUS-Frontend-Owner/src/api/RestaurantEndpoints.js
@@ -0,0 +1,28 @@
+import { get, post, put, destroy, patch } from './helpers/ApiRequestsHelper'
+function getAll () {
+  return get('users/myrestaurants')
+}
+
+function getDetail (id) {
+  return get(`restaurants/${id}`)
+}
+
+function getRestaurantCategories () {
+  return get('restaurantCategories')
+}
+
+function create (data) {
+  return post('restaurants', data)
+}
+
+function update (id, data) {
+  return put(`restaurants/${id}`, data)
+}
+
+function remove (id) {
+  return destroy(`restaurants/${id}`)
+}
+function changeStatus (id) {
+  return patch(`restaurants/${id}/status`)
+}
+export { getAll, getDetail, getRestaurantCategories, create, update, remove, changeStatus }
